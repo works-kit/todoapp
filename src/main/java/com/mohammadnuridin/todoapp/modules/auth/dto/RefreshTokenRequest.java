0da642e7043ec8908a0cc8e269f49024ae8d66cc
test1 <test1@gmail.com>
2026-03-07 03:26:55 +0700

    init

--- src/main/java/com/mohammadnuridin/todoapp/modules/auth/dto/RefreshTokenRequest.java
+++
@@ -1,9 +0,0 @@
-package com.mohammadnuridin.todoapp.modules.auth.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record RefreshTokenRequest(
-
-        @NotBlank(message = "{user.refresh.token.invalid}")
-        String refreshToken
-) {}
--- src/main/java/com/mohammadnuridin/todoapp/modules/user/domain/Role.java
+++
@@ -1,6 +0,0 @@
-package com.mohammadnuridin.todoapp.modules.user.domain;
-
-public enum Role {
-    USER,
-    ADMIN
-}
